27ae0c0d35dcfd23b5c10527eaadd332cc38bdd4
SaraSn01 <snizks00@jcu.cz>
2025-10-28 21:00:24 +0000

    první úkol

--- first.py
+++ first.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+def sudy_nebo_lichy (cislo):
+    if cislo % 2 == 0:
+        return (f"Číslo {cislo} je sudé.")
+    else:
+        return (f"Číslo {cislo} je liché.")
+    
+if __name__ =="__main__":
+    print(sudy_nebo_lichy(5))
+    print(sudy_nebo_lichy(1000000))
+
